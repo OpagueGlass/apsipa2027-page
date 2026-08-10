b8189f97f9b5197ccf683262da755a185a120e3a
Jona0002 <jona0002@student.monash.edu>
2026-08-10 11:19:59 +0800

    Removes archive block

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react'
 
 import type { Page } from '@/payload-types'
 
-import { ArchiveBlock } from '@/blocks/ArchiveBlock/Component'
 import { CallToActionBlock } from '@/blocks/CallToAction/Component'
 import { ContentBlock } from '@/blocks/Content/Component'
 import { FormBlock } from '@/blocks/Form/Component'
